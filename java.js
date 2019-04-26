200c246cee36ca4f6a85b894dbfff3da88af896b
Esaken <kenesau4763@gmail.com>
2019-04-26 17:08:54 +0300

    added images

---
+++ java.js
@@ -0,0 +1,7 @@
+function myFunction(){
+var Side1 = parseInt(document.getElementById("Side1").value);
+var Side2 = parseInt(document.getElementById("Side2").value);
+var Side3 = parseInt(document.getElementById("Side3").value);
+
+
+}
