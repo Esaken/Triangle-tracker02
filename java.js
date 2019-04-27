9ebce1a8222a421f5590858bee7b18e3d2fae00e
Esaken <kenesau4763@gmail.com>
2019-04-27 14:08:00 +0300

    deleted my javascript code

--- java.js
+++ java.js
@@ -1,14 +1,19 @@
-document.getElementById("submit").onclick = function() {myFunction()};
+document.getElementById("submit").onclick = myFunction();
+
 function myFunction(){
+  
 
   var Side1 = parseInt(document.getElementById("Side1").value);
   var Side2 = parseInt(document.getElementById("Side2").value);
   var Side3 = parseInt(document.getElementById("Side3").value);
   };
-  if(Side1>=Side2+Side3 || Side2>=Side3+Side1 || Side3>=Side2+Side1) {
-    document.getElementById('click').innerHTML ='Not a triangle';
+  ife(Side1>=Side2+Side3 || Side2>=Side3+Side1 || Side3>=Side2+Side1) {
+
+    document.getElementById('submit').innerHTML ='Not a triangle';
 
   };
-   else if (Side1===Side2 && Side2===Side3) {
-    document.getElementById('click').innerHTML ='Equilateral triangle';
-}
+   else (Side1===Side2 && Side2===Side3) {
+    document.getElementById('submit').innerHTML ='Equilateral triangle'
+    };
+
+};
