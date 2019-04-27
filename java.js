b0d29d133965c69ac48ecd23534f81d7c4639bea
Esaken <kenesau4763@gmail.com>
2019-04-27 13:25:58 +0300

    still encountering problems with my java

--- java.js
+++ java.js
@@ -1,7 +1,14 @@
+document.getElementById("submit").onclick = function() {myFunction()};
 function myFunction(){
-var Side1 = parseInt(document.getElementById("Side1").value);
-var Side2 = parseInt(document.getElementById("Side2").value);
-var Side3 = parseInt(document.getElementById("Side3").value);
 
+  var Side1 = parseInt(document.getElementById("Side1").value);
+  var Side2 = parseInt(document.getElementById("Side2").value);
+  var Side3 = parseInt(document.getElementById("Side3").value);
+  };
+  if(Side1>=Side2+Side3 || Side2>=Side3+Side1 || Side3>=Side2+Side1) {
+    document.getElementById('click').innerHTML ='Not a triangle';
 
+  };
+   else if (Side1===Side2 && Side2===Side3) {
+    document.getElementById('click').innerHTML ='Equilateral triangle';
 }
