a72a4ad8530b0c8417aebbc6e09db2e7e9404f64
Esaken <kenesau4763@gmail.com>
2019-04-27 23:17:35 +0300

    saturdays checkpoint

--- js/script.js
+++ js/script.js
@@ -1,20 +1,22 @@
-function triangle() {
-  var sides = ["Side1", "Side2", "Side3", "text"]
+function submit() {
+var Side1 = parseInt(documents.getElementById("Side1").value);
+var Side2 = parseInt(documents.getElementById("Side2").value);
+var Side3 = parseInt(documents.getElementById("Side3").value);
 if (Side1 + Side2 <= Side3 || Side1 + Side3 <= Side2 || Side2 + Side3 <= Side1){
-  text = document.getElementById('result').innerHTML = "This is not a triangle!";
+text = document.getElementById('result').innerHTML = "This is not a triangle!";
 }
-    else if (Side1 === Side2 && Side1 === Side3 && Side2 === Side3){
-      text = document.getElementById('result').innerHTML = "This is an equilateral triangle";
-    }
-    else if (Side1 === Side2 || Side1 === Side3 || Side2 === Side3){
-      text = document.getElementById('result').innerHTML = "This is an isosceles triangle";
-    }
-    else if (Side1**2 + Side2**2 === Side3**2){
-      text = document.getElementById('result').innerHTML = "This is a right-angled triangle.";
-    }
-    else if (Side1**2 + Side3**2 === Side2**2){
-      text = document.getElementById('result').innerHTML = "This is a right-angled triangle.";
-    }
-    else {
-      text = document.getElementById('result').innerHTML = "This is a scalene triangle." ;
-    }
+else if (Side1 === Side2 && Side1 === Side3 && Side2 === Side3){
+text = document.getElementById('result').innerHTML = "This is an equilateral triangle";
+}
+else if (Side1 === Side2 || Side1 === Side3 || Side2 === Side3){
+text = document.getElementById('result').innerHTML = "This is an isosceles triangle";
+}
+else if (Side1**2 + Side2**2 === Side3**2){
+text = document.getElementById('result').innerHTML = "This is a right-angled triangle.";
+}
+else if (Side1**2 + Side3**2 === Side2**2){
+text = document.getElementById('result').innerHTML = "This is a right-angled triangle.";
+}
+else {
+text = document.getElementById('result').innerHTML = "This is a scalene triangle." ;
+      }
