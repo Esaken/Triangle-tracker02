5b6aa035c92f309511d240a0324ad6280557f5a5
Esaken <kenesau4763@gmail.com>
2019-04-27 14:44:21 +0300

    java does not work

--- java.js
+++ java.js
@@ -1,19 +1,14 @@
-document.getElementById("submit").onclick = myFunction();
+function triangle() {
+  var Side1 = parseInt(document.getElementById('Side1'),value);
+  var Side2 = parseInt(document.getElementById('Side2'),value);
+  var Side3 = parseInt(document.getElementById('Side3'),value);
+  if (Side3>=Side2 + Side1 || Side2>= Side3 + Side1 || Side1>= Side2 + Side3) {
+    alert("This cannot form a  Triangle");
 
-function myFunction(){
-  
-
-  var Side1 = parseInt(document.getElementById("Side1").value);
-  var Side2 = parseInt(document.getElementById("Side2").value);
-  var Side3 = parseInt(document.getElementById("Side3").value);
-  };
-  ife(Side1>=Side2+Side3 || Side2>=Side3+Side1 || Side3>=Side2+Side1) {
-
-    document.getElementById('submit').innerHTML ='Not a triangle';
-
-  };
-   else (Side1===Side2 && Side2===Side3) {
-    document.getElementById('submit').innerHTML ='Equilateral triangle'
-    };
-
-};
+  }else if(Side1===Side2 && Side2===Side3){
+    alert("This is an Equilateral Triangle");
+  }else if (Side1===Side2 || Side2===Side3 || Side1===Side3) {
+    alert("This is an Isosceles Triangle");
+  }else {
+    alert("This is a Scalene Triangle");
+  }
