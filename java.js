99836d6f56a9cda5e695d2def37ec233bfc789fd
Esaken <kenesau4763@gmail.com>
2019-04-27 16:09:42 +0300

    trying out a new idea onmy java

--- java.js
+++ java.js
@@ -1,14 +1,20 @@
 function triangle() {
-  var Side1 = parseInt(document.getElementById('Side1'),value);
-  var Side2 = parseInt(document.getElementById('Side2'),value);
-  var Side3 = parseInt(document.getElementById('Side3'),value);
-  if (Side3>=Side2 + Side1 || Side2>= Side3 + Side1 || Side1>= Side2 + Side3) {
-    alert("This cannot form a  Triangle");
-
-  }else if(Side1===Side2 && Side2===Side3){
-    alert("This is an Equilateral Triangle");
-  }else if (Side1===Side2 || Side2===Side3 || Side1===Side3) {
-    alert("This is an Isosceles Triangle");
-  }else {
-    alert("This is a Scalene Triangle");
-  }
+  var sides = ["Side1", "Side2", "Side3", "text"]
+if (Side1 + Side2 <= Side3 || Side1 + Side3 <= Side2 || Side2 + Side3 <= Side1){
+  text = document.getElementById('result').innerHTML = "This is not a triangle!";
+}
+    else if (Side1 === Side2 && Side1 === Side3 && Side2 === Side3){
+      text = document.getElementById('result').innerHTML = "This is an equilateral triangle";
+    }
+    else if (Side1 === Side2 || Side1 === Side3 || Side2 === Side3){
+      text = document.getElementById('result').innerHTML = "This is an isosceles triangle";
+    }
+    else if (Side1**2 + Side2**2 === Side3**2){
+      text = document.getElementById('result').innerHTML = "This is a right-angled triangle.";
+    }
+    else if (Side1**2 + Side3**2 === Side2**2){
+      text = document.getElementById('result').innerHTML = "This is a right-angled triangle.";
+    }
+    else {
+      text = document.getElementById('result').innerHTML = "This is a scalene triangle.";
+    }
