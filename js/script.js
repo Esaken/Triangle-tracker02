fc98c2dc5ee4b699e42412f2057708cf00b4a1ff
Esaken <kenesau4763@gmail.com>
2019-04-29 09:56:09 +0300

    few changes on js

--- js/script.js
+++ js/script.js
@@ -1,22 +1,22 @@
 function myFunction(){
-   var inputs = document.getElementsByClassName("triangle-value");
-   var Side1=parseInt(inputs[0].value);
-   var Side2=parseInt(inputs[1].value);
-   var Side3=parseInt(inputs[2].value);
+   var inputs = document.getElementsByFormName("sides");
+   var Side1=parseInt(document.getElementById('id=Side1').value);
+   var Side2=parseInt(document.getElementById('id=Side2').value);
+   var Side3=parseInt(document.getElementById('id=Side3').value);
 
 if(Side2 + Side1<=Side3 && Side3 + Side1<=Side2 && Side3 + Side2<=Side1){
-document.getElementById('result').innerHTML = "DOESNT QUALIFY TO BE A TRIANGLE";
+      document.getElementById('result').innerHTML = "DOESNT QUALIFY TO BE A TRIANGLE";
 }
 if(Side1<=0 && Side2<=0 &&  Side3<=0){
       document.getElementById("result").innerText = "NOT A TRIANGLE";
-  }
+}
 else if(Side1===Side2 && Side1===Side3 && Side3 ===Side2){
-document.getElementById('result').innerHTML = "THIS IS AN EQUILATERAL TRIANGLE";
+      document.getElementById('result').innerHTML = "THIS IS AN EQUILATERAL TRIANGLE";
 }
 else if(Side2===Side1 || Side2===Side3 || Side3===Side1){
-  document.getElementById('result').innerHTML = "THIS IS AN ISOSCELES TRIANGLE";
+      document.getElementById('result').innerHTML = "THIS IS AN ISOSCELES TRIANGLE";
 }
 else if(Side2 + Side1>Side3 || Side3 + Side1>Side2 || Side3 + Side2>Side1){
-document.getElementById('result').innerHTML = "THIS IS A SCALENE TRIANGLE";
+      document.getElementById('result').innerHTML = "THIS IS A SCALENE TRIANGLE";
 }
 }
