33e442fb7a9d19360940929a6c3483089e1822ee
Esaken <kenesau4763@gmail.com>
2019-04-29 11:29:37 +0300

    resolved errors

--- js/script.js
+++ js/script.js
@@ -1,22 +1,22 @@
-function myFunction(){
-   var inputs = document.getElementsByFormName("sides");
-   var Side1=parseInt(document.getElementById('id=Side1').value);
-   var Side2=parseInt(document.getElementById('id=Side2').value);
-   var Side3=parseInt(document.getElementById('id=Side3').value);
+function myfunction() {
 
-if(Side2 + Side1<=Side3 && Side3 + Side1<=Side2 && Side3 + Side2<=Side1){
-      document.getElementById('result').innerHTML = "DOESNT QUALIFY TO BE A TRIANGLE";
-}
-if(Side1<=0 && Side2<=0 &&  Side3<=0){
-      document.getElementById("result").innerText = "NOT A TRIANGLE";
-}
-else if(Side1===Side2 && Side1===Side3 && Side3 ===Side2){
-      document.getElementById('result').innerHTML = "THIS IS AN EQUILATERAL TRIANGLE";
-}
-else if(Side2===Side1 || Side2===Side3 || Side3===Side1){
-      document.getElementById('result').innerHTML = "THIS IS AN ISOSCELES TRIANGLE";
-}
-else if(Side2 + Side1>Side3 || Side3 + Side1>Side2 || Side3 + Side2>Side1){
-      document.getElementById('result').innerHTML = "THIS IS A SCALENE TRIANGLE";
-}
+      var Side1 = parseInt(document.getElementById('Side1').value);
+      var Side2 = parseInt(document.getElementById('Side2').value);
+      var Side3 = parseInt(document.getElementById('Side3').value);
+
+      if (Side2 + Side1 <= Side3 && Side3 + Side1 <= Side2 && Side3 + Side2 <= Side1) {
+            document.getElementById('result').innerHTML = "DOESNT QUALIFY TO BE A TRIANGLE";
+      }
+      if (Side1 <= 0 && Side2 <= 0 && Side3 <= 0) {
+            document.getElementById("result").innerText = "NOT A TRIANGLE";
+      }
+      else if (Side1 === Side2 && Side1 === Side3 && Side3 === Side2) {
+            document.getElementById('result').innerHTML = "THIS IS AN EQUILATERAL TRIANGLE";
+      }
+      else if (Side2 === Side1 || Side2 === Side3 || Side3 === Side1) {
+            document.getElementById('result').innerHTML = "THIS IS AN ISOSCELES TRIANGLE";
+      }
+      else if (Side2 + Side1 > Side3 || Side3 + Side1 > Side2 || Side3 + Side2 > Side1) {
+            document.getElementById('result').innerHTML = "THIS IS A SCALENE TRIANGLE";
+      }
 }
